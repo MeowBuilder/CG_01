76816b6ee2860cc4430472f39798f9a10a63c1d8
TaesoonKim@Home <tta122057@gmail.com>
2024-10-03 00:47:37 +0900

    07 end

--- 07.cpp
+++ 07.cpp
@@ -3,8 +3,15 @@
 #include <vector>
 #include <stdlib.h>
 #include <stdio.h>
+#include <random>
 
 using namespace std;
+
+random_device seeder;
+const auto seed = seeder.entropy() ? seeder() : time(nullptr);
+mt19937 eng(static_cast<mt19937::result_type>(seed));
+uniform_int_distribution<int> sel_shape(0, 3);
+uniform_real_distribution<float> rend_color(0, 1);
 
 //--- 필요한 헤더파일 선언
 #include <gl/glew.h>
@@ -44,7 +51,7 @@ GLvoid drawScene();
 GLvoid Reshape(int w, int h);
 GLvoid Keyboard(unsigned char key, int x, int y);
 GLvoid Mouse(int button, int state, int x, int y);
-
+void move_shape(int selected, float dx, float dy);
 
 //--- 필요한 변수 선언
 GLint width, height;
@@ -60,6 +67,12 @@ vector<float> point_vertex;
 vector<float> line_vertex;
 vector<float> tri_vertex;
 vector<float> rect_vertex;
+
+int rect_count = 0;
+int shape_count = 0;
+int selected_shape = 0;
+int selected = 0;
+
 
 GLvoid InitBuffer()
 {
@@ -214,13 +227,9 @@ GLvoid drawScene() //--- 콜백 함수: 그리기 콜백 함수
 		glEnableVertexAttribArray(0);
 		glEnableVertexAttribArray(1);
 
-		for (int i = 0; i <= rect_vertex.size()/6; i++)
-		{
-			if (i % 4 == 0)
-			{
-				glDrawArrays(GL_TRIANGLE_STRIP, i, i + 4);//뭔가이상함
-			}
-		}
+		int Draw_vertex = (rect_vertex.size() / 6) / 6;
+
+		glDrawArrays(GL_TRIANGLES, 0, 6 * Draw_vertex);
 		// 각 점의 XYZ 좌표
 	}
 
@@ -255,25 +264,135 @@ GLvoid Keyboard(unsigned char key, int x, int y) {
 		cur_state = key;
 		break;
 	case 'w':
-
+		selected_shape = sel_shape(eng);
+		move_shape(selected_shape, 0, 0.5f);
 		break;
 	case 'a':
-		cout << point_vertex.size();
+		selected_shape = sel_shape(eng);
+		move_shape(selected_shape, - 0.5f, 0);
 		break;
 	case 's':
-
+		selected_shape = sel_shape(eng);
+		move_shape(selected_shape, 0, -0.5f);
 		break;
 	case 'd':
-
+		selected_shape = sel_shape(eng);
+		move_shape(selected_shape, 0.5f, 0);
 		break;
 	case 'c':
+		point_vertex.clear();
+		line_vertex.clear();
+		tri_vertex.clear();
+		rect_vertex.clear();
 
+		rect_count = 0;
+		shape_count = 0;
+		selected_shape = 0;
+		selected = 0;
 		break;
 	case 'q':
 		glutLeaveMainLoop();
 		break;
 	}
 	glutPostRedisplay();
+}
+
+bool is_in_rect(float x,float y) {
+	bool x_in = !(x < -1.0f || x > 1.0f);
+	bool y_in = !(y < -1.0f || y > 1.0f);
+	return (x_in && y_in);
+}
+
+void move_shape(int selected, float dx,float dy) {
+	bool bad_for_start = true;
+	while (bad_for_start)
+	{
+		switch (selected)
+		{
+		case 0://점
+			if ((point_vertex.size() / 6) > 0) bad_for_start = false;
+			break;
+		case 1://선
+			if (((line_vertex.size() / 6) / 2) > 0) bad_for_start = false;
+			break;
+		case 2://삼각형
+			if (((tri_vertex.size() / 6) / 3) > 0) bad_for_start = false;
+			break;
+		case 3://사각형
+			if (((rect_vertex.size() / 6) / 6) > 0) bad_for_start = false;
+			break;
+		}
+		if (!bad_for_start) break;
+		selected = sel_shape(eng);
+	}
+
+
+	uniform_int_distribution<int> random(0, 100);
+	int ver_sel = 0;
+	int mid_x, mid_y;
+	switch (selected)
+	{
+	case 0://점
+		ver_sel = random(eng) % (point_vertex.size() / 6);
+		mid_x = point_vertex[((ver_sel + 0) * 6)];
+		mid_y = point_vertex[((ver_sel + 0) * 6) + 1];
+
+		if (!is_in_rect(mid_x + dx, mid_y + dy)) break;
+
+		point_vertex[((ver_sel + 0) * 6)] += dx;
+		point_vertex[((ver_sel + 0) * 6) + 1] += dy;
+
+		break;
+	case 1://선
+		ver_sel = random(eng) % ((line_vertex.size() / 6) / 2);
+		mid_x = (line_vertex[((ver_sel + 0) * 6)] + line_vertex[((ver_sel + 1) * 6)]) / 2;
+		mid_y = (line_vertex[((ver_sel + 0) * 6) + 1] + line_vertex[((ver_sel + 1) * 6) + 1]) / 2;
+
+		if (!is_in_rect(mid_x + dx, mid_y + dy)) break;
+
+		line_vertex[((ver_sel + 0) * 6)] += dx;
+		line_vertex[((ver_sel + 0) * 6) + 1] += dy;
+		line_vertex[((ver_sel + 1) * 6)] += dx;
+		line_vertex[((ver_sel + 1) * 6) + 1] += dy;
+		break;
+	case 2://삼각형
+		ver_sel = random(eng) % ((tri_vertex.size() / 6) / 3);
+
+		mid_x = (tri_vertex[((ver_sel + 0) * 6)] + tri_vertex[((ver_sel + 1) * 6)] + tri_vertex[((ver_sel + 2) * 6)]) / 3;
+		mid_y = (tri_vertex[((ver_sel + 0) * 6) + 1] + tri_vertex[((ver_sel + 1) * 6) + 1] + tri_vertex[((ver_sel + 2) * 6) + 1]) / 3;
+
+		if (!is_in_rect(mid_x + dx, mid_y + dy)) break;
+
+		tri_vertex[((ver_sel + 0) * 6)] += dx;
+		tri_vertex[((ver_sel + 0) * 6) + 1] += dy;
+		tri_vertex[((ver_sel + 1) * 6)] += dx;
+		tri_vertex[((ver_sel + 1) * 6) + 1] += dy;
+		tri_vertex[((ver_sel + 2) * 6)] += dx;
+		tri_vertex[((ver_sel + 2) * 6) + 1] += dy;
+		break;
+	case 3://사각형
+		ver_sel = random(eng) % ((rect_vertex.size() / 6) / 6);
+
+		mid_x = (rect_vertex[((ver_sel + 0) * 6)] + rect_vertex[((ver_sel + 1) * 6)] + rect_vertex[((ver_sel + 2) * 6)] + rect_vertex[((ver_sel + 3) * 6)]) / 4;
+		mid_y = (rect_vertex[((ver_sel + 0) * 6) + 1] + rect_vertex[((ver_sel + 1) * 6) + 1] + rect_vertex[((ver_sel + 2) * 6) + 1] + rect_vertex[((ver_sel + 3) * 6) + 1]) / 4;
+
+		if (!is_in_rect(mid_x + dx, mid_y + dy)) break;
+
+		rect_vertex[((ver_sel + 0) * 6)] += dx;
+		rect_vertex[((ver_sel + 0) * 6) + 1] += dy;
+		rect_vertex[((ver_sel + 1) * 6)] += dx;
+		rect_vertex[((ver_sel + 1) * 6) + 1] += dy;
+		rect_vertex[((ver_sel + 2) * 6)] += dx;
+		rect_vertex[((ver_sel + 2) * 6) + 1] += dy;
+		rect_vertex[((ver_sel + 3) * 6)] += dx;
+		rect_vertex[((ver_sel + 3) * 6) + 1] += dy;
+
+		rect_vertex[((ver_sel + 4) * 6)] += dx;
+		rect_vertex[((ver_sel + 4) * 6) + 1] += dy;
+		rect_vertex[((ver_sel + 5) * 6)] += dx;
+		rect_vertex[((ver_sel + 5) * 6) + 1] += dy;
+		break;
+	}
 }
 
 void Mouse(int button, int state, int x, int y)
@@ -284,36 +403,63 @@ void Mouse(int button, int state, int x, int y)
 		switch (cur_state)
 		{
 		case 'p':
+			if (shape_count > 9) break;
 			point_vertex.push_back(cx);
 			point_vertex.push_back(cy);
 			point_vertex.push_back(0.0f);
-			point_vertex.push_back(1.0f);
-			point_vertex.push_back(0.0f);
-			point_vertex.push_back(0.0f);
+			point_vertex.push_back(rend_color(eng));
+			point_vertex.push_back(rend_color(eng));
+			point_vertex.push_back(rend_color(eng));
+			shape_count++;
 			break;
 		case 'l':
+			if (shape_count > 9) break;
 			line_vertex.push_back(cx);
 			line_vertex.push_back(cy);
 			line_vertex.push_back(0.0f);
-			line_vertex.push_back(1.0f);
-			line_vertex.push_back(0.0f);
-			line_vertex.push_back(0.0f);
+			line_vertex.push_back(rend_color(eng));
+			line_vertex.push_back(rend_color(eng));
+			line_vertex.push_back(rend_color(eng));
+			if ((line_vertex.size() / 6)%2 == 0) shape_count++;
 			break;
 		case 't':
+			if (shape_count > 9) break;
 			tri_vertex.push_back(cx);
 			tri_vertex.push_back(cy);
 			tri_vertex.push_back(0.0f);
-			tri_vertex.push_back(1.0f);
-			tri_vertex.push_back(0.0f);
-			tri_vertex.push_back(0.0f);
+			tri_vertex.push_back(rend_color(eng));
+			tri_vertex.push_back(rend_color(eng));
+			tri_vertex.push_back(rend_color(eng));
+			if ((tri_vertex.size() / 6) % 3 == 0) shape_count++;
 			break;
 		case 'r':
+			if (shape_count > 9) break;
 			rect_vertex.push_back(cx);
 			rect_vertex.push_back(cy);
 			rect_vertex.push_back(0.0f);
-			rect_vertex.push_back(1.0f);
-			rect_vertex.push_back(0.0f);
-			rect_vertex.push_back(0.0f);
+			rect_vertex.push_back(rend_color(eng));
+			rect_vertex.push_back(rend_color(eng));
+			rect_vertex.push_back(rend_color(eng));
+			rect_count++;
+			if (rect_count == 4)
+			{
+				rect_vertex.push_back(rect_vertex[rect_vertex.size() - 24]);
+				rect_vertex.push_back(rect_vertex[rect_vertex.size() - 24]);
+				rect_vertex.push_back(rect_vertex[rect_vertex.size() - 24]);
+				rect_vertex.push_back(rect_vertex[rect_vertex.size() - 24]);
+				rect_vertex.push_back(rect_vertex[rect_vertex.size() - 24]);
+				rect_vertex.push_back(rect_vertex[rect_vertex.size() - 24]);
+
+				rect_vertex.push_back(rect_vertex[rect_vertex.size() - 18]);//이전 버텍스의 마지막
+				rect_vertex.push_back(rect_vertex[rect_vertex.size() - 18]);
+				rect_vertex.push_back(rect_vertex[rect_vertex.size() - 18]);
+				rect_vertex.push_back(rect_vertex[rect_vertex.size() - 18]);
+				rect_vertex.push_back(rect_vertex[rect_vertex.size() - 18]);
+				rect_vertex.push_back(rect_vertex[rect_vertex.size() - 18]);
+
+				rect_count = 0;
+				shape_count++;
+			}
 			break;
 		}
 	}
